edbe465d17b36df2b1cd098e421467dd7b1827dc
mediapills <mediapills@gmail.com>
2021-10-06 22:42:28 +0300

    Bump version 0.1.0 -> 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 # Copyright The Mediapills Dependency Injection Authors.
 # SPDX-License-Identifier: MIT
 import setuptools
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
-version = "0.1.0"
+version = "0.1.1"
 
-setuptools.setup(version=version)
+requirements = parse_requirements("requirements.txt", session=PipSession())
+
+install_requires = [str(requirement.req) for requirement in requirements]  # type: ignore
+
+setuptools.setup(version=version, install_requires=install_requires)
